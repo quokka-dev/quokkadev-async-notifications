3a2807f3e596d12ac4ff4e8aca522650dd1b8a6c
Fabio Cometti <fabio.cometti@gmail.com>
2022-06-28 14:47:18 +0200

    fix: fix smell code
    fix ISerializable implementation for NotificationException

--- src/QuokkaDev.AsyncNotifications.Abstractions/Exceptions/NotificationException.cs
+++ src/QuokkaDev.AsyncNotifications.Abstractions/Exceptions/NotificationException.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.Serialization;
 
 namespace QuokkaDev.AsyncNotifications.Abstractions.Exceptions
 {
     /// <summary>
     /// Exception throwed by notification handlers
     /// </summary>
+    [System.Serializable]
     public class NotificationException : ApplicationException
     {
         public NotificationException() : base()
@@ -21,6 +18,17 @@ namespace QuokkaDev.AsyncNotifications.Abstractions.Exceptions
 
         public NotificationException(string? message, Exception? innerException) : base(message, innerException)
         {
+        }
+
+        protected NotificationException(SerializationInfo serializationInfo, StreamingContext streamingContext) : base(serializationInfo, streamingContext)
+        {
+            // No custom attribute to add in serialization
+        }
+
+        public override void GetObjectData(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context)
+        {
+            // No custom attribute to add in serialization
+            base.GetObjectData(info, context);
         }
     }
 }
